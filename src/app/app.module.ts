13526d8cef8b9bade48200ebce916584718bd61d
Ruslan Fazliev <ruslan1fazliev@gmail.com>
2019-03-24 11:53:48 +0100

    Deleted extra files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { TreeTestComponent } from './tree-test/tree-test.component';
 import { TreeComponent } from './tree/tree.component';
 import { TreeService } from './tree/tree.service';
 import { QuillModule } from 'ngx-quill';
 import { HttpClientModule } from "@angular/common/http";
-import { RightClickMenuComponent } from './right-click-menu/right-click-menu.component';
 import { RadialMenuComponent } from './radial-menu/radial-menu.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SupporterComponent } from './supporter/supporter.component';
-import { QuestionEditComponent } from './question-edit/question-edit.component';
-import { AnswersEditComponent } from './answers-edit/answers-edit.component';
-import { InformationEditComponent } from './information-edit/information-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HelperComponent, NgbdModal2Content } from './helper/helper.component';
-import { TooltipComponent } from './tooltip/tooltip.component';
 import { AddInformationComponent } from './add-information/add-information.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserServiceComponent } from './user-service/user-service.component';
-import { GeneralInformationEditComponent } from './general-information-edit/general-information-edit.component';
 import { NodeEditorComponent } from './node-editor/node-editor.component';
 import { TreeManagerComponent } from './tree-manager/tree-manager.component';
 import { StudentViewComponent } from './student-view/student-view.component';
@@ -38,21 +30,13 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    TreeTestComponent,
     TreeComponent,
-    RightClickMenuComponent,
     RadialMenuComponent,
     ProfileComponent,
-    SupporterComponent,
-    QuestionEditComponent,
-    AnswersEditComponent,
-    InformationEditComponent,
     HelperComponent,
-    TooltipComponent,
     AddInformationComponent,
     UserLoginComponent,
     UserServiceComponent,
-    GeneralInformationEditComponent,
     NodeEditorComponent,
     TreeManagerComponent,
     StudentViewComponent,
@@ -73,9 +57,6 @@ const appRoutes: Routes = [
     ReactiveFormsModule
   ],
   entryComponents: [
-    QuestionEditComponent,
-    AnswersEditComponent,
-    InformationEditComponent,
     TreeComponent,
     NodeEditorComponent,
     TreeManagerComponent,
